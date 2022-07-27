c4e0beeceb35a374feb2f76f75ec220950515b02
awoldt <abreckwoldt23@gmail.com>
2022-07-26 23:16:58 -0400

    no imgs next to news articles, pages load faster

--- src/interfaces/stock_news.ts
+++ src/interfaces/stock_news.ts
@@ -1,7 +1,6 @@
 export default interface stock_news {
     ticker: string | null;
     title: string | null;
-    image: string | null;
     site: string | null;
     text: string | null;
     url: string | null;
